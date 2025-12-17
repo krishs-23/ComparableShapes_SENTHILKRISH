e53d6b0da08e9432fa335c842f33d4e135b09e1e
Bethany Kankelborg <bkankelborg@lwsd.org>
2025-12-17 08:48:20 -0800

    Populating starter project: Part 2

---
+++ ComparableShapes/src/ShapeTester.java
@@ -0,0 +1,14 @@
+/**
+ * ShapeTester.java
+ * 
+ * Name: 
+ * Period:
+ * Last Revision Date:
+ * Description: 
+ */
+
+public class ShapeTester {
+  public static void main(String[] args) {
+    
+  }
+}

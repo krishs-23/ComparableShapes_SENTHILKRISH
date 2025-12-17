5295f02ae41b3c43a7fbb5a9b931b2ece6c84675
1090456 <1090456@487E304146.lwsd.k12wa.org>
2025-12-17 08:58:42 -0800

    Adding the Shape Interface

---
+++ ComparableShapes/src/Shape.java
@@ -0,0 +1,13 @@
+/**
+ * Shape.java
+ * 
+ * Name: Krish Senthil	
+ * Period: 2
+ * Last Revision Date: 12/17/2025
+ * Description: 
+ */
+
+public interface Shape extends Comparable<Shape> {
+	public double getArea();
+	public double getPerimeter();
+}
--- ComparableShapes/src/ShapeTester.java
+++ ComparableShapes/src/ShapeTester.java
@@ -9,6 +9,6 @@
 
 public class ShapeTester {
   public static void main(String[] args) {
-    
+	  
   }
 }
